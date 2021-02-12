eb9992aca94ac67e9fa485c87207acddbceea147
Guillermo Saavedra <guillermo.witideal@gmail.com>
2021-02-12 17:02:37 -0600

    Initial Commit

---
+++ ExamenIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamenIOS
+//
+//  Created by Guillermo Saavedra Dorantes on 12/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
